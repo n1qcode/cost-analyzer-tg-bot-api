5dd30e53d8e425bdbe121cfb8ee20bd48836dae4
Ncode <n1kvd@yandex.ru>
2023-05-16 21:38:30 +0300

    feat(controller): added costController

---
+++ controller/cost.controller.ts
@@ -0,0 +1,13 @@
+export class CostController {
+  async createCost(req, res) {}
+  async addToCost(req, res) {}
+  async getCurrentCost(req, res) {}
+  async getYearCost(req, res) {}
+  async getSeasonCost(req, res) {}
+  async getMonthCost(req, res) {}
+  async getDayCost(req, res) {}
+  async getAllCost(req, res) {}
+  async getPeriodCost(req, res) {}
+}
+
+export default new CostController();
