2987c2dc5bbf62ce4378bcf4666a5d317033014a
Ncode <n1kvd@yandex.ru>
2023-05-18 18:36:53 +0300

    feat: fixed cost routes

--- routes/cost.routes.ts
+++ routes/cost.routes.ts
@@ -8,20 +8,23 @@ router.put("/cost", CostController.createCostCategory);
 
 router.put("/user/cost/amount", CostController.addToCostCategory);
 
-router.get("/user/:id/cost/current", CostController.getCurrentDayCostOfUser);
-router.get("/cost/current", CostController.getCurrentDayCost);
-
 router.get("/user/:id/cost", CostController.getAllCostOfUser);
 router.get("/cost", CostController.getAllCost);
 
-router.get("/user/:id/cost/:year", CostController.getYearCostOfUser);
-router.get("/cost/:year", CostController.getYearCost);
+router.get("/user/:id/cost/year/:year", CostController.getYearCostOfUser);
+router.get("/cost/year/:year", CostController.getYearCost);
 
-router.get("/user/:id/cost/:season", CostController.getSeasonCostOfUser);
-router.get("/cost/:season", CostController.getSeasonCost);
+router.get("/user/:id/cost/season/:season", CostController.getSeasonCostOfUser);
+router.get("/cost/season/:season", CostController.getSeasonCost);
 
-router.get("/user/:id/cost/:month", CostController.getMonthCostOfUser);
-router.get("/cost/:month", CostController.getMonthCost);
+router.get(
+  "/user/:id/cost/year/:year/month/:month",
+  CostController.getMonthCostOfUser
+);
+router.get("/cost/year/:year/month/:month", CostController.getMonthCost);
+
+router.get("/user/:id/cost/day/:date", CostController.getDayCostOfUser);
+router.get("/cost/day/:date", CostController.getDayCost);
 
 router.get("/user/:id/cost/:start/:end", CostController.getPeriodCostOfUser);
 router.get("/cost/:start/:end", CostController.getPeriodCost);
