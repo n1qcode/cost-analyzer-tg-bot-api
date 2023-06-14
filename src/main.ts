a0ddf33370548db1aad6dae30305f866e8586b21
Nick <n1kvd@yandex.ru>
2023-06-14 18:13:29 +0300

    feat: removed deprecated user router and controller

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import express, { Express } from "express";
 import config from "config";
 
-import userRouter from "../routes/user.routes";
 import costRouter from "../routes/cost.routes";
 import TranslationRouter from "../routes/translation.routes";
 import FrequencyRouter from "../routes/frequency.routes";
@@ -15,7 +14,6 @@ const app: Express = express();
 
 app.use(loggerMiddleware);
 app.use(express.json());
-app.use("/api", userRouter);
 app.use("/api", costRouter);
 app.use("/api", TranslationRouter);
 app.use("/api", FrequencyRouter);
