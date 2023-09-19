4b1fc856cc210b3881132344a72fbadeb2366987
Nick <n1kvd@yandex.ru>
2023-09-19 14:32:32 +0300

    feat: added general prefix "/finance" to finance routes

--- routes/moneyBox.routes.ts
+++ routes/moneyBox.routes.ts
@@ -4,8 +4,8 @@ import MoneyBoxController from "../controller/moneyBox.controller";
 
 const router = Router();
 
-router.get("/money_box", MoneyBoxController.getInfoOfMoneyBox);
-router.put("/money_box/put", MoneyBoxController.putMoneyToMoneyBox);
-router.put("/money_box/take", MoneyBoxController.takeMoneyFromMoneyBox);
+router.get("finance/money_box", MoneyBoxController.getInfoOfMoneyBox);
+router.put("finance/money_box/put", MoneyBoxController.putMoneyToMoneyBox);
+router.put("finance/money_box/take", MoneyBoxController.takeMoneyFromMoneyBox);
 
 export default router;
